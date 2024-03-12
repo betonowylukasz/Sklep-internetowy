c79e73e5ddc6148582736001b2e448c3fa1c3479
Łukasz Wasilewski <122639243+betonowylukasz@users.noreply.github.com>
2024-03-12 03:33:36 +0100

    Add files via upload

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using lista10.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace lista10.Data
+{
+    public class MyDbContext: IdentityDbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Article> Article { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Order> Order {  get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>()
+                .HasOne(a => a.Category)
+                .WithMany()
+                .HasForeignKey(a => a.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+    }
+}
---
+++ Data/MyIdentityDataInitializer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace lista10.Data
+{
+    public class MyIdentityDataInitializer
+    {
+        public static async Task SeedData(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+        // name - poprawny adres email
+        // password - min 8 znaków, mała i duża litera, cyfra i znak specjalny
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "Admin",
+                };
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Pracownik").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "Pracownik",
+                };
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+        }
+        public static void SeedOneUser(UserManager<IdentityUser> userManager, string name, string password, string role = null)
+        {
+            if (userManager.FindByNameAsync(name).Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = name, // musi być taki sam jak email, inaczej nie zadziała
+                    Email = name
+                };
+                IdentityResult result = userManager.CreateAsync(user, password).Result;
+                if (result.Succeeded && role != null)
+                {
+                    userManager.AddToRoleAsync(user, role).Wait();
+                }
+            }
+        }
+        public static async Task SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            SeedOneUser(userManager, "normaluser@localhost", "User123!");
+            SeedOneUser(userManager, "adminuser@localhost", "Admin123!", "Admin");
+            SeedOneUser(userManager, "workeruser@localhost", "Worker123!", "Pracownik");
+        }
+
+    }
+}
